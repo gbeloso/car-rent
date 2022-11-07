eae166a6d5b2e401d01aaf3330b9b93d3bb02ec6
Belos <g.beloso@hotmail.com>
2022-11-07 18:03:55 -0300

    Consult a rent test passed

--- car.py
+++ car.py
@@ -1,6 +1,12 @@
+from datetime import date
 class Car:
     def __init__(self, brand, model, year, day_tax):
         self.brand = brand
         self.model = model
         self.year = year
         self.day_tax = day_tax
+
+    def consult_rent(self, date_init, date_final):
+        di = date.fromisoformat(date_init)
+        df = date.fromisoformat(date_final)
+        return((df-di).days * self.day_tax * 1.15)
--- tests.py
+++ tests.py
@@ -6,3 +6,7 @@ def test_rent_car():
     testCar = Car('Chevrolet', 'Prisma', 2022, 15.00)
     testUser.rent_car(testCar, '2022-11-05', '2022-11-08')
     assert testUser.rented_car == testCar
+
+def test_consult_rent():
+    testCar = Car('Chevrolet', 'Prisma', 2022, 15.00)
+    assert round(testCar.consult_rent('2022-11-05', '2022-11-08'), 2) == 51.75
